275d9abd423bea133f855d6af48794adebcead5d
jecongg <jasontjendika@gmail.com>
2024-06-07 16:26:34 +0700

    tes

--- Alpro/src/alpro/PlayPanel.java
+++ Alpro/src/alpro/PlayPanel.java
@@ -18,17 +18,76 @@ public class PlayPanel extends JPanel {
     BufferedImage api;
     BufferedImage es;
     BufferedImage player;
-    int tileSize = 50;
+    int tileSize = 70;
     int worldPlayerX, worldPlayerY, screenPlayerX, screenPlayerY;
-    int startPlayerX, startPlayerY;
+    int startPlayerX, startPlayerY, currentX, currentY;
+    int tambahY[]={-1,0,1,0};
+    int tambahX[]={0,1,0,-1};
+    boolean isVisited[][];
 
     public PlayPanel() {
         bacaFile();
         importGambar();
         screenPlayerX=(714/2) -(tileSize/2);
         screenPlayerY=(508/2) -(tileSize/2);
-        worldPlayerX=3*tileSize;
-        worldPlayerY=3*tileSize;
+
+        currentX=startPlayerX;
+        currentY=startPlayerY;
+        isVisited=new boolean[map.length][map[0].length];
+        worldPlayerX=startPlayerX*tileSize;
+        worldPlayerY=startPlayerY*tileSize;
+        
+        System.out.println(backtrack(startPlayerX, startPlayerY, 0, 4));
+    }
+    
+    public int backtrack(int x, int y, int langkah, int lives){
+        if(lives<=0){
+            return-1;
+        }
+        else if(map[y][x]=='g'){
+            return langkah;
+        }
+        else{
+            int tempLangkah=0;
+//            System.out.println(langkah);
+            boolean cek=false;
+            boolean pertama=true;
+            for(int i=0; i<4; i++){
+                int yTemp=y+tambahY[i];
+                int xTemp=x+tambahX[i];
+                if(xTemp>=0 && yTemp>=0 && xTemp<map[0].length && yTemp<map.length){
+                    if(!isVisited[yTemp][xTemp] && map[yTemp][xTemp]!='b'){
+                        isVisited[yTemp][xTemp]=true;
+                        int temp=0;
+                        if(map[yTemp][xTemp]=='a'){
+                            temp=backtrack(xTemp, yTemp, langkah+1, lives-1);
+                        }
+                        else if(map[yTemp][xTemp]=='h'){
+                            temp=backtrack(xTemp, yTemp, langkah+1, lives+1);
+                        }
+                        else{
+                            temp=backtrack(xTemp, yTemp, langkah+1, lives);
+                        }
+                        if(pertama && temp!=-1 && temp!=0){
+                            tempLangkah=temp;
+                            cek=true;
+                            pertama=false;
+                        }
+                        else{
+                            if(temp<tempLangkah && temp!=-1){
+                                tempLangkah=temp;
+                            }
+                        }
+                        isVisited[yTemp][xTemp]=false;
+                    }
+                }
+            }
+            if(cek){
+                return tempLangkah;
+            }
+            return -1;
+            
+        }
     }
 
     public void importGambar() {
@@ -43,7 +102,7 @@ public class PlayPanel extends JPanel {
     }
     
     public void atas(){
-        worldPlayerY-=50;
+        worldPlayerY-=tileSize;
         repaint();
     }
 
@@ -59,6 +118,10 @@ public class PlayPanel extends JPanel {
             for (int i = 0; i < map.length; i++) {
                 for (int j = 0; j < map[0].length; j++) {
                     map[i][j] = fileBentukString.get(i).charAt(j);
+                    if(map[i][j]=='s'){
+                        startPlayerX=j;
+                        startPlayerY=i;
+                    }
                 }
             }
         } catch (FileNotFoundException ex) {
