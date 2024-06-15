647a21069fc078995ce80d78b57a72830f94088b
jecongg <jasontjendika@gmail.com>
2024-06-15 19:28:33 +0700

    tambah nama file dalam folder

--- Alpro/src/alpro/Alpro.java
+++ Alpro/src/alpro/Alpro.java
@@ -1,14 +1,25 @@
-
 package alpro;
 
+import java.io.File;
 
 public class Alpro {
 
     public static void main(String args[]) {
 //        Editor e = new Editor();
 //        e.main();
+        File folder = new File("src/File");
+        File[] listOfFiles = folder.listFiles();
+        if (listOfFiles != null) {
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    System.out.println("File " + listOfFiles[i].getName());
+                } else if (listOfFiles[i].isDirectory()) {
+                    System.out.println("Directory " + listOfFiles[i].getName());
+                }
+            }
+        }
         MainMenu e = new MainMenu();
         e.main();
     }
-    
+
 }
